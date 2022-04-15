c509fb4d41eeff55f5a1980ad8be85de4a4766fc
karpss <ola.fam10@gmail.com>
2022-04-15 22:53:35 +0100

    basic functionality implemented

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable */
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import QuestionPage from '../QuestionPage/QuestionPage';
 import HomePageProps from '../../interface/homePageProps';
 import AnswerProps from '../../interface/answerProps.interface';
 import {getQuiz} from '../../http/http';
-import {totalSetQuestions, difficultyLevel} from '../../constants/constants'
+import {totalSetQuestions, difficultyLevel} from '../../constants/constants';
 import './HomePage.css';
 
 const HomePage: React.FC = () => {
@@ -30,22 +30,65 @@ const HomePage: React.FC = () => {
 
   },[])
 
-  console.log(questions);
+  //console.log(questions);
 
-  const checkTheAnswer = () => {
-    console.log('Answer Checked');
+  const checkTheAnswer = (e: React.FormEvent <HTMLFormElement>): void => {
+    e.preventDefault();
+    if (gameOver) return;
+    // selected answer
+    const choosedAnswer = e.currentTarget.innerText;
+    // Matching chosen answer with correct answer from the API
+    const correct = questions[questionNumber]?.correct_answer === choosedAnswer;
+    // score tracking
+    if (correct) setScore((previous) => previous + 1);
+    // user can change choice
+    if (userAnswer.length != questionNumber) {
+      if (!correct) setScore((previous) => previous - 1);
+      const lastIndex = userAnswer.length - 1;
+      if (lastIndex >= 0) {
+        userAnswer.splice(lastIndex, 1);
+        const answerObject = {
+          question: questions[questionNumber]?.question,
+          answer: choosedAnswer,
+          correct,
+          correctAnswer: questions[questionNumber]?.correct_answer,
+        };
+        setUserAnswer((previous) => [...previous, answerObject]);
+      }
+      return;
+
+    }
+// answer interface
+    const answerObject = {
+      question: questions[questionNumber]?.question,
+      answer: choosedAnswer,
+      correct,
+      correctAnswer: questions[questionNumber]?.correct_answer,
+    };
+    // spread the previous answer and add to the answer object to update
+    setUserAnswer((previous) => [...previous, answerObject]);
   }
 
   const startTheQuiz = (): void => {
     setStartQuiz(true);
   }
 
-  const nextQuestion = (): void => {
-
+  const nextQuestion = (e: React.FormEvent<HTMLButtonElement>): void => {
+    e.preventDefault();
+    const nextQuestion = questionNumber + 1;
+    if (totalSetQuestions === nextQuestion) {
+      setGameOver(true);
+      return;
+    }
+    setQuestionNumber(nextQuestion);
   }
 
   const replayTheQuiz = (): void => {
-    console.log('replay quiz');
+    setStartQuiz(false);
+    setGameOver(false);
+    setQuestionNumber(0);
+    setScore(0);
+    setUserAnswer([]);
   }
 
 
@@ -84,7 +127,29 @@ const HomePage: React.FC = () => {
         
         
         />
+        <button className='next-button' onClick={nextQuestion}>Next Question</button>
         </>
+        )}
+
+        {gameOver && (
+          <>
+          <h2>Game Over!</h2>
+          
+          <p>Your score is {score}/{totalSetQuestions}</p>
+          
+          
+          <button className='next-button' onClick={replayTheQuiz}>Replay Quiz</button>
+          
+          
+          
+          
+          </>
+
+
+
+
+
+
         )}
         </div>
 
